9289244c1125c2971e3f2e82dee99ac9c9a70b3c
bazo <martin@bazo.sk>
2012-07-16 03:41:38 +0200

    moved some methods

--- Facebook/Graph/Response.php
+++ Facebook/Graph/Response.php
@@ -40,25 +40,4 @@ class Response extends \Facebook\Responses\Response
 		}
 		$this->frozen = true;
 	}
-	
-	public function __get($property)
-	{
-		if(!isset($this->$property))
-		{
-			return null;
-		}
-		return $this->$property;
-	}
-	
-	public function __set($property, $value)
-	{
-		if($this->frozen === true)
-		{
-			throw new Exception(sprintf('Class %s is read-only.', get_class($this)));
-		}
-		else
-		{
-			$this->$property = $value;
-		}
-	}
 }
--- Facebook/Responses/Response.php
+++ Facebook/Responses/Response.php
@@ -12,4 +12,25 @@ abstract class Response
 		$frozen = false
 	;
 	
+	public function __get($property)
+	{
+		if(!isset($this->$property))
+		{
+			return null;
+		}
+		return $this->$property;
+	}
+	
+	public function __set($property, $value)
+	{
+		if($this->frozen === true)
+		{
+			throw new Exception(sprintf('Class %s is read-only.', get_class($this)));
+		}
+		else
+		{
+			$this->$property = $value;
+		}
+	}
+	
 }
